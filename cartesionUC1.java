a9969664f1b57728d7e55941357533b9a2ebbe89
avinashpatil <patilavinash223@gmail.com>
2021-11-23 09:38:06 +0530

    add one file cartesion file

---
+++ cartesionUC1.java
@@ -0,0 +1,12 @@
+public class cartesionUC1 {
+
+	/*int x1 =10,x2 =20, y1 =15, y2=40;*/
+    public static void main (String[] args) {
+
+	 int x1 =10,x2 =20, y1 =15, y2=40;
+
+         double length=Math.sqrt((x2-x1)^2 + (y2 -y1)^2);
+
+	System.out.println("length of line: " + length);
+	}
+}
